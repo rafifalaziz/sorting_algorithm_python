b8682e1ef069ce2e1caf1f0a014de2f45808f13a
rafifkyodo <rafif@kyodo-i.com>
2023-11-09 11:41:11 +0700

    bubble

---
+++ bubble.py
@@ -0,0 +1,9 @@
+def bubble(arr):
+  sortedIndex = len(arr)-1
+  for i in range(len(arr)):
+    for j in range(sortedIndex):
+      if(arr[j]>arr[j+1]):
+        arr[j], arr[j+1] = arr[j+1], arr[j]
+    sortedIndex-=1
+    print(arr)
+  return arr
